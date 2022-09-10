1e518dad8ebbba2f83cb52f601626b6960a24f81
xixo-tan <lilo93choco@icloud.com>
2022-09-11 03:02:38 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function ($) {
+  $('.bg-switcher').bgSwitcher({
+    images: ['../images/background1.jpg', '../images/background2.jpg', '../images/background3.jpg', '../images/background4.jpg', '../images/background5.jpg'],
+    interval: 4000,
+    loop: true
+  })
+
+  $(window).scroll(function () {
+    var targetElement = $('.fadein').offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > targetElement) {
+      $('.fadein').css('opacity', '1');
+    }
+  });
+
+  $('.slider').slick({
+    autoplay: false,
+    slidesToShow:4,
+    infinite:true,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    centerPadding: '0',
+
+  });
+
+
+});
