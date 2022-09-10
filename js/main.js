f5991a4a12e74dede399c81a2c5784bcb627e67b
xixo-tan <lilo93choco@icloud.com>
2022-09-11 06:53:13 +0900

    responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function ($) {
+$(document).ready(function() {
   $('.bg-switcher').bgSwitcher({
     images: ['../images/background1.jpg', '../images/background2.jpg', '../images/background3.jpg', '../images/background4.jpg', '../images/background5.jpg'],
     interval: 4000,
@@ -14,16 +14,26 @@ $(function ($) {
     }
   });
 
-  $('.slider').slick({
-    autoplay: false,
-    slidesToShow:4,
-    infinite:true,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: true,
-    centerPadding: '0',
-
-  });
-
-
+  var windowWidth = $(window).width();
+  if (windowWidth > 480) {
+    $('.slider').slick({
+      autoplay: false,
+      slidesToShow: 4,
+      infinite: true,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      centerPadding: '0',
+    });
+  } else {
+    $('.slider').slick({
+      autoplay: false,
+      slidesToShow: 1,
+      infinite: true,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      centerPadding: '0',
+    });
+  }
 });
